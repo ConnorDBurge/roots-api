05f69a9d391c116c5bc9579b4fb9817404fc3487
Connor D. Burge <connor.d.burge@gmail.com>
2023-03-23 14:40:04 -0400

    minor changes

--- src/handler.js
+++ src/handler.js
@@ -7,11 +7,5 @@ import { server } from "./server.mjs";
 
 export const handler = startServerAndCreateLambdaHandler(
   server,
-  // We will be using the Proxy V2 handler
   handlers.createAPIGatewayProxyEventV2RequestHandler()
 );
-
-/**
- * @TODO Secure /graphql with Cognito user pool
- * @TODO Deploy with GitHub Actions
- */
