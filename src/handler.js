72de0aa9a95c4d85e3424ac5f071d07cf22eee03
Connor D. Burge <connor.d.burge@gmail.com>
2023-03-24 13:22:36 -0400

    new handler

--- src/handler.js
+++ src/handler.js
@@ -5,11 +5,25 @@ import {
 
 import { server } from "./server.js";
 
-export const handler = startServerAndCreateLambdaHandler(server, {
-  context: async ({ event, context }) => {
-    return {
-      lambdaEvent: event,
-      lambdaContext: context,
-    };
-  },
-});
+const requestHandler = handlers.createAPIGatewayProxyEventV2RequestHandler();
+
+// Middleware is an async function whose type is based on your request handler. Middleware
+// can read and mutate the incoming event. Additionally, returning an async function from your
+// middleware allows you to read and mutate the result before it's sent.
+const middlewareFn = async (event) => {
+  // read or update the event here
+  // optionally return a callback to access the result
+  console.log({ event });
+  return async (result) => {
+    // read or update the result here
+    console.log({ result });
+  };
+};
+
+export const handler = startServerAndCreateLambdaHandler(
+  server,
+  requestHandler,
+  {
+    middleware: [middlewareFn],
+  }
+);
